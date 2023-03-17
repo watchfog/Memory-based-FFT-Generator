b333437fe6298e23be8ec8b6996afc48a945acfe
watchfog <1227400954@qq.com>
2023-03-17 21:17:55 +0800

    change config

--- src/main/scala/fft/FFTConfig.scala
+++ src/main/scala/fft/FFTConfig.scala
@@ -7,8 +7,8 @@ trait DataConfig{
     val fftLength = 8
     val fftDataWidth = 14
     val twiddleDataWidth = 12
-    val parallelCnt = 1
+    val parallelCnt = 3 // as it's radix-2^3
     val addrWidth = log2Ceil(fftLength)
-    val stageCnt = addrWidth - 1
+    val stageCnt = if(addrWidth % 3 == 0) addrWidth / 3 else addrWidth / 3 + 1
     val midBankSel = pow(2, parallelCnt - 1).toInt
 }
