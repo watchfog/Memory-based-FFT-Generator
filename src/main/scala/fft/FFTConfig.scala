0c316b9b7c75eda12b367bd64a2663e9f9d2c8d5
watchfog <1227400954@qq.com>
2023-04-18 10:17:31 +0800

    add comment to FFTConfig

--- src/main/scala/fft/FFTConfig.scala
+++ src/main/scala/fft/FFTConfig.scala
@@ -4,12 +4,15 @@ import chisel3.util.log2Ceil
 import scala.math._
 
 trait DataConfig{
-    val needProc = true
-    val fftLength = 128
-    val fftDataWidth = 14
-    val twiddleDataWidth = 12
-    val parallelCnt = 1
-    val addrWidth = log2Ceil(fftLength)
-    val stageCnt = addrWidth - 1
-    val midBankSel = pow(2, parallelCnt - 1).toInt
+    val needProc = true //need Proc stage and twiddle LUT for RFFT/HSIFFT
+    val fftLength = 128 //must be 2^n and greater than 8
+    val fftDataWidth = 14 //only tested 14 and above, thought it will work
+    val twiddleDataWidth = 12 //tested 14 and 12, thought it will work
+    val parallelCnt = 1 //2^(parallelCnt - 1) calc units only 1-4 was implemented
+    val addrWidth = log2Ceil(fftLength) //how many bits is addr
+    val stageCnt = addrWidth - 1 // how many stage
+    val midBankSel = pow(2, parallelCnt - 1).toInt //parameter for RFFT
 }
+
+
+
